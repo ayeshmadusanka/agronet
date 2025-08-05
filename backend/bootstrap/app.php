a5156895e842cd5fc625dc7f42c9f62410608221
ayeshmadusanka <amjmadusanka@gmail.com>
2025-08-05 20:03:18 +0530

    Cors error fixed

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\EnsureJsonResponse::class,
         ]);
         
